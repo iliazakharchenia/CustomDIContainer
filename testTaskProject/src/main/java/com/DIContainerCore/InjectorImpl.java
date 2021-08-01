122ffc5c82982e77337c6f808de3c0709ea95df8
Ilia Zakharchenia <iliazakh97@gmail.com>
2021-08-01 19:04:52 +0300

    Add exceptions trowing into InjectorImpl

--- testTaskProject/src/main/java/com/DIContainerCore/InjectorImpl.java
+++ testTaskProject/src/main/java/com/DIContainerCore/InjectorImpl.java
@@ -1,12 +1,24 @@
 package com.DIContainerCore;
 
+import com.Annotations.Inject;
+import com.Exceptions.ConstructorNotFoundException;
+import com.Exceptions.TooManyConstructorsException;
 import com.Interfaces.Injector;
 import com.Interfaces.Provider;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 
 public class InjectorImpl implements Injector {
 
     @Override
-    public <T> Provider<T> getProvider(Class<T> type) {
+    public <T> Provider<T> getProvider(Class<T> type) throws ConstructorNotFoundException,
+                                                                TooManyConstructorsException {
+        if (hasMultipleInjections(type)) {
+            throw new TooManyConstructorsException(" Founded more than one constructor with @Inject ");
+        }
+
         return null;
     }
 
@@ -18,5 +30,32 @@ public class InjectorImpl implements Injector {
     @Override
     public <T> void bindSingleton(Class<T> intf, Class<? extends T> impl) {
 
+    }
+
+    private ArrayList<Annotation> getContructorsInjectAnnotations(Class injectedClass) {
+        Constructor<?>[] constructors = injectedClass.getConstructors();
+        ArrayList<Annotation> annotations = new ArrayList<Annotation>();
+        for (int i=0; i<constructors.length; i++) {
+            if (constructors[i].getAnnotation(Inject.class) != null) {
+                annotations.add(constructors[i].getAnnotation(Inject.class));
+            }
+        }
+
+        return annotations;
+    }
+
+    private boolean isNoSuchInjections(Class injectedClass) {
+        ArrayList<Annotation> annotations = getContructorsInjectAnnotations(injectedClass);
+
+        return annotations.size() == 0;
+    }
+
+    private boolean hasMultipleInjections(Class injectedClass) throws ConstructorNotFoundException {
+        if (isNoSuchInjections(injectedClass)) {
+            throw new ConstructorNotFoundException(" No constructor with @Inject found ");
+        }
+        ArrayList<Annotation> annotations = getContructorsInjectAnnotations(injectedClass);
+
+        return annotations.size() > 1;
     }
 }
--- testTaskProject/src/main/java/com/Interfaces/Injector.java
+++ testTaskProject/src/main/java/com/Interfaces/Injector.java
@@ -1,8 +1,11 @@
 package com.Interfaces;
 
+import com.Exceptions.ConstructorNotFoundException;
+import com.Exceptions.TooManyConstructorsException;
+
 public interface Injector {
 
-    <T> Provider<T> getProvider(Class<T> type);
+    <T> Provider<T> getProvider(Class<T> type) throws ConstructorNotFoundException, TooManyConstructorsException;
 
     <T> void bind(Class<T> intf, Class<? extends T> impl);
 
