2bccc731a8c58aadb82ec4b1eff14cdeb8eae622
Ilia Zakharchenia <iliazakh97@gmail.com>
2021-08-02 22:24:48 +0300

    Implement Provider

--- testTaskProject/src/main/java/com/DIContainerCore/ProviderImpl.java
+++ testTaskProject/src/main/java/com/DIContainerCore/ProviderImpl.java
@@ -3,9 +3,15 @@ package com.DIContainerCore;
 import com.Interfaces.Provider;
 
 public class ProviderImpl <T> implements Provider {
+    private T instance;
+
+    ProviderImpl (T instance) {
+        this.instance = instance;
+    }
 
     @Override
-    public T getInstance() {
-        return null;
+    public T getInstance() throws NullPointerException {
+        if (instance == null) { throw new NullPointerException("Instance reference is not defined"); }
+        return instance;
     }
 }
